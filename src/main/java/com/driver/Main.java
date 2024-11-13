469b2d662160d6951101e27590b3c4b43a37b936
chandan-chintu <109058973+chandan-chintu@users.noreply.github.com>
2024-11-13 21:11:13 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
+
+   public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+
+        //Try setting a value to name by directly accessing it using obj and also try printing it
+        // rwOnly.name="Rahul";
+        //'name' has private access in 'com.driver.RWOnly'
+
+        rwOnly.setName("Rahul");
+        System.out.println(rwOnly.getName());
+
+    }
   
 }
